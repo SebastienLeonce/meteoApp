c411050ad3559e2162c2c3b4bc39611e5fb24dd3
Sébastien LEONCE <sebastien.leonce@cea.fr>
2021-10-30 12:07:06 +0200

    add balmui framework

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
 import "./registerServiceWorker";
+
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import BalmUI from "balm-ui";
+import BalmUIPlus from "balm-ui/dist/balm-ui-plus";
+import BalmUINext from "balm-ui/dist/balm-ui-next";
+
+createApp(App)
+  .use(BalmUI)
+  .use(BalmUIPlus)
+  .use(BalmUINext)
+  .use(store)
+  .use(router)
+  .mount("#app");
